a1c6d47fc929a507e89c54e7349c9130e000bea1
mozilla <luke.pacholski@gmail.com>
2020-04-30 22:28:46 -0500

    First commit - flip

---
+++ flip/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    const pawn = document.querySelector('.pawn-body');
+    const wrapper = document.querySelector('.pawn-wrapper');
+    const shadow = document.querySelector('.pawn-shadow');
+
+    const animationClasses = ["jump", "flip", "land"]
+    let animationIndex = 0;
+
+    pawn.addEventListener('click', (e) => {
+        pawn.classList.add(animationClasses[0])
+    });
+
+    pawn.addEventListener('animationend', (e) => {
+
+        animationIndex++;
+        let animClass = animationClasses[animationIndex];
+        pawn.classList.add(animClass);
+
+        if(animClass == "flip") {
+            wrapper.classList.add("moveUp");
+            shadow.classList.add("shadowMoveUp");
+        }
+    });
+});
