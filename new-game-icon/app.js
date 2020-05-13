16ed497256478eef44e41041f3e78ccfbe025cc6
mozilla <luke.pacholski@gmail.com>
2020-05-12 23:31:47 -0500

    Added an :active style for the icon as well

--- new-game-icon/app.js
+++ new-game-icon/app.js
@@ -1,17 +1,5 @@
-let tile;
-
 document.addEventListener("DOMContentLoaded", function(event) { 
 
-    tile = document.querySelector(".tile");
-
-    tile.addEventListener("mouseenter", e => {
-        e.target.classList.add("hover");
-    });
-
-    tile.addEventListener("mouseleave", e => {
-        e.target.classList.remove("hover");
-    });
 
 
 });
-
